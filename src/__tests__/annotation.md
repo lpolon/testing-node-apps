ee798efdabc87fcbf13fe249c18e977f338394e6
lpolon <lpolon@gmail.com>
2020-04-10 16:29:54 -0300

    exercise 1 done

---
+++ src/__tests__/annotation.md
@@ -0,0 +1,6 @@
+# auth.exercise
+- a abstração do casify é muito legal. usa key: value e Object.entries.
+
+- pra ter intellisense do jest:   `npm i -D @types/jest`
+
+- o ponto é que para funções puras muito complexas, podemos colocar os test cases de input numa lista.
--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -1,21 +1,63 @@
-// Testing Pure Functions
+import {isPasswordAllowed} from '../auth'
 
-// 💣 remove this todo test (it's only here so you don't get an error about missing tests)
-test.todo('remove me')
 
-// 🐨 import the function that we're testing
-// 💰 import {isPasswordAllowed} from '../auth'
+describe('isPasswordAllowed function', () => {
+  it('accepts valid password', () => {
+    const validPw = '!aBc123'
 
-// 🐨 write tests for valid and invalid passwords
-// 💰 here are some you can use:
-//
-// valid:
-// - !aBc123
-//
-// invalid:
-// - a2c! // too short
-// - 123456! // no alphabet characters
-// - ABCdef! // no numbers
-// - abc123! // no uppercase letters
-// - ABC123! // no lowercase letters
-// - ABCdef123 // no non-alphanumeric characters
+    const result = isPasswordAllowed(validPw)
+
+    expect(result).toBe(true)
+  })
+  it('must have seven or more chars', () => {
+    const tooShortPw = '!aBc12'
+
+    const result = isPasswordAllowed(tooShortPw)
+
+    expect(result).toBe(false)
+  })
+  it('must have alphabet chars', () => {
+    const noABCPw = '!123456'
+    const result = isPasswordAllowed(noABCPw)
+    expect(result).toBe(false)
+  })
+  it('must have Numbers', () => {
+    const noNumbersPw = '!aBCdfg'
+    const result = isPasswordAllowed(noNumbersPw)
+    expect(result).toBe(false)
+  })
+  it('must have uppercase letters', () => {
+    const noUpperCasePw = '!abc123'
+    const result = isPasswordAllowed(noUpperCasePw)
+    expect(result).toBe(false)
+  })
+  it('must have lowercase letters', () => {
+    const noLowerCasePw = '!ABC123'
+    const result = isPasswordAllowed(noLowerCasePw)
+    expect(result).toBe(false)
+  })
+  it('must have non-alphanumerical chars', () => {
+    const noNonAlphaNumChars = 'ABc1234'
+    const result = isPasswordAllowed(noNonAlphaNumChars)
+    expect(result).toBe(false)
+  })
+})
+
+// reduce duplication;
+
+it('must pass all validation rules', () => {
+  const validationRules = [
+    {
+      description: 'must have saven or more chars',
+      setupInput: '!aBc12',
+    },
+    {
+      description: 'must have lowercase letters',
+      setupInput: '!ABC123',
+    },
+  ]
+  validationRules.forEach(({description, setupInput}) => {
+    const result = isPasswordAllowed(setupInput)
+    if(result === true) throw Error(description)
+  })
+})
